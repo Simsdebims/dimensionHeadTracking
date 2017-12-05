2ed0adc8bf35b9e95b570cf03e22248416fd6ac0
Simon Kappe <simon.kappe@kit.edu>
2017-12-05 13:43:34 +0100

    .

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
+//#include <opencv2/viz.hpp>
 
 #include "server.h"
 
@@ -17,9 +18,9 @@ using namespace libfreenect2;
 typedef Freenect2Device::IrCameraParams DepthCameraParams;
 typedef Freenect2Device::ColorCameraParams ColorCameraParams;
 
-struct BoundingBox {
+struct TrackingBox {
 
-    BoundingBox(string id, float minX, float maxX, float minY, float maxY, float minZ, float maxZ) :
+    TrackingBox(string id, float minX, float maxX, float minY, float maxY, float minZ, float maxZ) :
             id(id), minX(minX), maxX(maxX), minY(minY), maxY(maxY), minZ(minZ), maxZ(maxZ) { }
 
     bool isInside(cv::Point3f& p) {
@@ -39,15 +40,33 @@ struct BoundingBox {
         return false;
     }
 
+    void reset() {
+        top = cv::Point3f();
+        points.clear();
+    }
+
+    void refine(float topRange = 0.05f) {
+        float thresh = top.y - topRange;
+        cv::Point3f sum;
+        int cnt = 0;
+        for (auto& p : points) {
+            if (p.y > thresh) {
+                sum += p;
+                cnt++;
+            }
+        }
+        top = sum / cnt;
+    }
+
     float minX, maxX, minY, maxY, minZ, maxZ;
-    cv::Point3f top{0.0f, 0.0f, 0.0f};
+    cv::Point3f top;
     vector<cv::Point3f> points;
     string id;
 };
 
-cv::Point3f unprojectPoint(float depth, int u, int v, DepthCameraParams& params) {
+cv::Point3f unprojectPoint(float rawDepth, int u, int v, DepthCameraParams& params) {
 
-    float z = depth / 1000.0f;
+    float z = rawDepth / 1000.0f;
     float x = z * (u - params.cx) / params.fx;
     float y = z * (v - params.cy) / params.fy;
 
@@ -55,7 +74,7 @@ cv::Point3f unprojectPoint(float depth, int u, int v, DepthCameraParams& params)
 }
 
 void transformAndSplit(Frame* depthFrame, DepthCameraParams& params, cv::Affine3f transformation, float zThresh,
-                       vector<BoundingBox>& boxes) {
+                       vector<TrackingBox>& boxes) {
 
     float thresh = zThresh * 1000.0f;
 
@@ -80,29 +99,42 @@ bool
 findTransformation(cv::Mat& colorImage, cv::Mat& registeredDepthImage, DepthCameraParams& depthParams,
                    cv::Affine3f& result) {
 
-    cv::Ptr<cv::aruco::Dictionary> dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250); // TODO
+    // Find markers in rgb image
+
+    cv::Ptr<cv::aruco::Dictionary> dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
     vector<vector<cv::Point2f>> corners;
     vector<int> ids;
 
-    while (corners.size() < 4) {
-        corners.clear();
-        ids.clear();
-        cv::aruco::detectMarkers(colorImage, dict, corners, ids);
-    }
+    cv::aruco::detectMarkers(colorImage, dict, corners, ids);
 
     if (corners.size() < 4) return false;
 
     cout << "Found markers." << endl;
 
-    vector<cv::Point3f> markers;
+    // Get 3D points for markers from depth data
 
+    vector<cv::Point3f> markers;
     for (int i = 0; i < 4; ++i) {
-        int u = (int) corners[i][0].x;
-        int v = (int) corners[i][0].y;
-        float d = ((float*) registeredDepthImage.data)[u + (v + 1) * 512];
-        if (d <= 0.0f || d > 10.0f) return false;
+        int c = i == 0 ? 3 : i - 1;
+        int u = (int) corners[i][c].x;
+        int v = (int) corners[i][c].y;
+        float d = registeredDepthImage.at<float>(v, u);
+        if (d <= 0.0f || d > 10000.0f) return false;
         markers.push_back(unprojectPoint(d, u, v, depthParams));
     }
+
+    for (auto& p : markers) {
+        cout << p << endl;
+        cv::aruco::drawDetectedMarkers(colorImage, corners, ids);
+        cv::aruco::drawDetectedMarkers(registeredDepthImage, corners, ids);
+    }
+
+    cv::imshow("markers1", colorImage);
+    cv::imshow("markers2", registeredDepthImage / 4096.0f);
+
+
+
+    // Compute transformation for center point
 
     cv::Point3f from = (markers[0] + markers[1] + markers[2] + markers[3]) / 4.0f;
     cv::Vec3f dir = cv::normalize(cv::Vec3f(markers[2] - markers[1]));
@@ -114,6 +146,7 @@ findTransformation(cv::Mat& colorImage, cv::Mat& registeredDepthImage, DepthCame
     cv::Vec3f newup = dir.cross(right);
 
     // Create transformation matrix
+
     cv::Matx44f transform = cv::Matx44f::eye();
     vector<cv::Vec3f> m = {right, newup, dir};
     cv::Mat3f r(m);
@@ -150,7 +183,6 @@ int main() {
 
     PacketPipeline* pipeline = 0;
 
-
     pipeline = new libfreenect2::CudaPacketPipeline;
 //    pipeline = new libfreenect2::OpenGLPacketPipeline;
 //    pipeline = new libfreenect2::CpuPacketPipeline();
@@ -161,7 +193,6 @@ int main() {
         std::cerr << "Failure opening kinect!" << std::endl;
         return -1;
     }
-
 
     SyncMultiFrameListener listener(libfreenect2::Frame::Color | Frame::Depth | libfreenect2::Frame::Ir);
     dev->setIrAndDepthFrameListener(&listener);
@@ -193,6 +224,25 @@ int main() {
     Server server;
     server.start();
 
+
+    vector<TrackingBox> boxes;
+    boxes.push_back({"B0", -1.5f, -0.5f, 0.0f, 2.5f, 0.0f, 1.0f});
+    boxes.push_back({"B1", -1.5f, -0.5f, 0.0f, 2.5f, -1.0f, 0.0f});
+    boxes.push_back({"B2", 0.5f, 1.5f, 0.0f, 2.5f, 0.0f, 1.0f});
+    boxes.push_back({"B3", 0.5f, 1.5f, 0.0f, 2.5f, -1.0f, 0.0f});
+
+
+    // Visualization
+//    cv::viz::Viz3d window3D("Viz");
+//    cv::viz::WPlane table_w(cv::Point3d(0, 0, 0), cv::Vec3d(0, 1, 0), cv::Vec3d(0, 0, 1), cv::Size2d(1, 2));
+//    window3D.showWidget("table", table_w);
+//    vector<cv::viz::WSphere> positions_w;
+//    for (auto& b: boxes) {
+//        cv::viz::WSphere s(cv::Point3d(0, 0, 0), 0.1);
+//        window3D.showWidget(b.id, s);
+//        positions_w.push_back(s);
+//    }
+
     signal(SIGINT, siginthandler);
 
     while (running) {
@@ -207,38 +257,47 @@ int main() {
 
         cv::Mat colorImage(color->height, color->width, CV_8UC4, color->data);
         cv::Mat depthImage(depth->height, depth->width, CV_32FC1, depth->data);
+        cv::Mat undistortedImage(depth->height, depth->width, CV_32FC1, depth->data);
         cv::Mat registeredImage(registered.height, registered.width, CV_8UC4, registered.data);
         cv::Mat depth2rgbImage(depth2rgb.height, depth2rgb.width, CV_32FC1, depth2rgb.data);
 
-        cv::Mat rgb;
-        cv::cvtColor(colorImage, rgb, CV_RGBA2RGB);
+        cv::Mat rgb, mappedDepth;
+        cv::cvtColor(registeredImage, rgb, CV_RGBA2RGB);
+        cv::flip(rgb, rgb, 1);
+        cv::flip(undistortedImage, mappedDepth, 1);
 
-/*
-        cv::imshow("Color", colorImage);
-        cv::imshow("Color2", rgb);
+        //cv::imshow("Color", colorImage);
+        // cv::imshow("Color2", rgb);
         cv::imshow("Registered", registeredImage);
-        cv::imshow("Depth", depthImage / 4096.0f);
-        cv::imshow("depth2rgb", depth2rgbImage / 4096.0f);
+        //cv::imshow("Depth", depthImage / 4096.0f);
+        // cv::imshow("depth2rgb", depth2rgbImage / 4096.0f);
         cv::waitKey(1);
-*/
-
 
         if (!calibrated) {
             cout << "Searching for markers" << endl;
-            cout << rgb.channels() << endl;
-            calibrated = findTransformation(rgb, depth2rgbImage, depthParams, transformation);
+            calibrated = findTransformation(rgb, mappedDepth, depthParams, transformation);
             listener.release(frames);
             continue;
         }
 
-/*
-        vector<BoundingBox> boxes;
-        boxes.push_back({"B0", -0.5f, -1.5f, 0.0f, 2.5f, 0.0f, 1.0f});
-        boxes.push_back({"B1", -0.5f, -1.5f, 0.0f, 2.5f, -1.0f, 0.0f});
-        boxes.push_back({"B2", 0.5f, 1.5f, 0.0f, 2.5f, 0.0f, 1.0f});
-        boxes.push_back({"B3", 0.5f, 1.5f, 0.0f, 2.5f, -1.0f, 0.0f});
+        transformation.translation(cv::Vec3f(0, 1.5f, -1.5f));
+
+        for (auto& b : boxes) b.reset();
+
         transformAndSplit(depth, depthParams, transformation, 5.0f, boxes);
-*/
+
+        for (auto& b : boxes) b.refine();
+
+        // Visualization
+//        for (auto& b : boxes) {
+//            cv::Point3f pos = cv::Vec3f(b.top);
+//            if (pos == cv::Point3f()) continue;
+//            cv::Affine3d pose;
+//            pose.translation(cv::Vec3f(b.top));
+//            window3D.setWidgetPose(b.id, pose);
+//        }
+//        window3D.spinOnce(1, true);
+
 
         listener.release(frames);
 /*
